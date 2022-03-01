452e61b897f959b0802c52a45c0abcf8850f1b14
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-01 15:31:01 -0600

    lesson 1

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS  just_tech_news_db;
+
+CREATE DATABASE  just_tech_news_db;
 DROP DATABASE IF EXISTS newsfeed_db;
 
 CREATE DATABASE newsfeed_db;
--- models/index.js
+++ models/index.js
@@ -10,33 +10,27 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id'
 });
 
 User.belongsToMany(Post, {
   through: Vote,
   as: 'voted_posts',
-
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id'
 });
 
 Post.belongsToMany(User, {
   through: Vote,
   as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'post_id'
 });
 
 Vote.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id'
 });
 
 Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'post_id'
 });
 
 User.hasMany(Vote, {
@@ -48,18 +42,15 @@ Post.hasMany(Vote, {
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id'
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'post_id'
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
